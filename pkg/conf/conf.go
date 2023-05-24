875615d69c05b8915db8a4aca8b424f9ff1288ff
Yevhen Horbachov <y.horbachov@gmail.com>
2023-05-24 16:33:10 +0200

    feat: add basic config module

---
+++ pkg/conf/conf.go
@@ -0,0 +1,24 @@
+package conf
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port           int
+	EndpointsPefix string
+}
+
+func NewDefaultConfig() Config {
+	v := viper.New()
+	v.AutomaticEnv()
+	v.SetEnvPrefix("COA")
+
+	v.SetDefault("PORT", 8080)
+	v.SetDefault("ENDPOINTS_PREFIX", "/oauth-agent")
+
+	return Config{
+		Port:           v.GetInt("PORT"),
+		EndpointsPefix: v.GetString("ENDPOINTS_PREFIX"),
+	}
+}
